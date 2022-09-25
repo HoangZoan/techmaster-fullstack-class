3493382067bb004849fe7c40c29889d3ee14cd6b
HoangZoan <d.hoang1906@gmail.com>
2022-09-25 23:47:21 +0700

    Update

--- src/js/main.js
+++ src/js/main.js
@@ -16,3 +16,43 @@ form.addEventListener("submit", (event) => {
 
   form.classList.add("was-validated");
 });
+
+// SWIPER
+const projectsSwiper = new Swiper(".student-projects-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  slidesPerGroup: 1,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+    },
+    1200: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+    },
+  },
+});
+
+const gallerySwiper = new Swiper(".gallery-swiper", {
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+//
+const scrollSpy = new bootstrap.ScrollSpy(document.body, {
+  target: "#header-nav",
+});
